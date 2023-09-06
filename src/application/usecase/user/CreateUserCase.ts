1243ffb914412d1e81f1cd0bc576329dfaf983ec
manderdeveloper <manderdeveloper@gmail.com>
2023-09-06 15:46:43 +0200

    feat (user): add ingredients for user in creation

--- src/application/usecase/user/CreateUserCase.ts
+++ src/application/usecase/user/CreateUserCase.ts
@@ -2,6 +2,9 @@ import { inject, injectable } from "inversify";
 import { UserRepository } from "../../../domain/repository/UserRepository";
 import { User } from "../../../domain/model/User";
 import { IngredientRepository } from "../../../domain/repository/IngredientRepository";
+import { INITIAL_INGREDIENTS } from "../../../infraestructure/config/IngredientsInitialization";
+import { Ingredient } from "../../../domain/model/Ingredient";
+import { UuidValueObject } from "../../../domain/valueobject/primitives/UuidValueObject";
 
 
 @injectable()
@@ -14,7 +17,11 @@ export class CreateUserUseCase {
   public async execute(body: any): Promise<any> {
     const user = User.fromPrimitives({...body})
     await this.userRepository.create(user);
-    
+    const initalIngredients = INITIAL_INGREDIENTS;
+    for (let rawIngredient of initalIngredients) {
+      const ingredient = Ingredient.fromPrimitives({id: UuidValueObject.random().value ,name: rawIngredient.name, isMain: rawIngredient.isMain, user: user.id.value})
+      await this.ingredientRepository.create(ingredient);
+    }
   }
 
 }
--- src/infraestructure/config/IngredientsInitialization.ts
+++ src/infraestructure/config/IngredientsInitialization.ts
@@ -95,3 +95,82 @@ export const INIT_INGREDIENTS = [
     "isMain": false
   }
 ];
+
+export const INITIAL_INGREDIENTS = [
+  {
+    "name": "Salmón",
+    "isMain": true
+  },
+  {
+    "name": "Pollo",
+    "isMain": true
+  },
+  {
+    "name": "Pimienta",
+    "isMain": false
+  },
+  {
+    "name": "Brócoli",
+    "isMain": false
+  },
+  {
+    "name": "Tomate",
+    "isMain": false
+  },
+  {
+    "name": "Cebolla",
+    "isMain": false
+  },
+  {
+    "name": "Zanahoria",
+    "isMain": false
+  },
+  {
+    "name": "Papa",
+    "isMain": false
+  },
+  {
+    "name": "Ajo",
+    "isMain": false
+  },
+  {
+    "name": "Cilantro",
+    "isMain": false
+  },
+  {
+    "name": "Canela",
+    "isMain": false
+  },
+  {
+    "name": "Limón",
+    "isMain": false
+  },
+  {
+    "name": "Albahaca",
+    "isMain": false
+  },
+  {
+    "name": "Perejil",
+    "isMain": false
+  },
+  {
+    "name": "Pimiento",
+    "isMain": false
+  },
+  {
+    "name": "Champiñones",
+    "isMain": false
+  },
+  {
+    "name": "Espárragos",
+    "isMain": false
+  },
+  {
+    "name": "Calabacín",
+    "isMain": false
+  },
+  {
+    "name": "Espinaca",
+    "isMain": false
+  }
+];
